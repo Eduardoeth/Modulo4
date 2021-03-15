9c746f28f4a7fb0f603cfc6e0c7ab9596cb5e0f1
edu.t.h@outlook.es <edu.t.h@outlook.es>
2021-03-15 00:57:08 -0300

    modulo

---
+++ app1/static/js/login.js
@@ -0,0 +1,13 @@
+
+function login(){
+    var user = document.getElementById("login-user");
+    var pass = document.getElementById("login-pass");
+
+    if ( user.value == "user@awake.com" && pass.value == "pass" ){
+        window.location.href="home.html";
+    }else{
+        alert("Error Usuario o Contraseña incorrecta");
+    }
+}
+
+    
